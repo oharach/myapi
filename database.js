081481cce854d74d1a03c2f7f844f1eeb9e4bff3
Ossama Harach <oharach@temenos.com>
2019-06-21 14:53:48 +0200

    [TASK] Users table correct schema

--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 var sqlite3 = require('sqlite3').verbose()
-var md5 = require('md5')
 
 let db = new sqlite3.Database("mydb.sqlite", (err) => {
     if (err) {
@@ -9,25 +8,21 @@ let db = new sqlite3.Database("mydb.sqlite", (err) => {
     }else{
         console.log('Connected to the SQLite database.')
         db.run(`CREATE TABLE Users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            email text UNIQUE, 
-            password text, 
-            CONSTRAINT email_unique UNIQUE (email)
+            userId INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstName text, 
+            lastName text, 
+            pictureURL text
             )`,
         (err) => {
             if (err) {
                 // Table already created
             }else{
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO Users (name, email, password) VALUES (?,?,?)'
-                db.run(insert, ["admin","admin@example.com",md5("admin123456")])
-                db.run(insert, ["user","user@example.com",md5("user123456")])
-                db.run(insert, ["ossama","ossama.harach@gmail.com",md5("ossama123456")])
+                var insert = 'INSERT INTO Users (firstName, lastName) VALUES (?,?)'
+                db.run(insert, ["Ossama","HARACH"])
+                db.run(insert, ["Raphael","BISCHOF"])
+                db.run(insert, ["Steve","JOBS"])
             }
         });  
     }
 });
-
-
-// module.exports = db
