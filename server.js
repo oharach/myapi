6c9b23cdf5c6c6bcf04adcf065ef5e9de848ca93
Ossama Harach <oharach@temenos.com>
2019-06-21 16:24:46 +0200

    [FEATURE] Patch and Delete endpoints

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require("express")
 var app = express()
 var sqlite3 = require('sqlite3').verbose()
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Connection
 let db = new sqlite3.Database("mydb.sqlite", sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
@@ -26,7 +30,7 @@ app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
 
-// API endpoints
+// GET
 app.get("/api/users", (req, res, next) => {
     var sql = "select * from Users"
     var params = []
@@ -41,6 +45,80 @@ app.get("/api/users", (req, res, next) => {
         })
       });
 });
+
+// POST
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.firstName){
+        errors.push("No first name specified");
+    }
+    if (!req.body.lastName){
+        errors.push("No last name specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        pictureURL: req.body.pictureURL
+    }
+    var sql ='INSERT INTO Users (firstName, lastName, pictureURL) VALUES (?,?,?)'
+    var params =[data.firstName, data.lastName, data.pictureURL]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
+
+// UPDATE
+app.patch("/api/user/:id", (req, res, next) => {
+    var data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        pictureURL : req.body.pictureURL
+    }
+    db.run(
+        `UPDATE Users set 
+           firstName = COALESCE(?,firstName), 
+           lastName = COALESCE(?,lastName), 
+           pictureURL = COALESCE(?,pictureURL) 
+           WHERE id = ?`,
+        [data.firstName, data.lastName, data.pictureURL, req.params.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
+})
+
+// DELETE
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM Users WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", changes: this.changes})
+    });
+})
 
 // Default response for any other request
 app.use(function(req, res){
