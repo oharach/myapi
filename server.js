c0a9229c946c37f231d321d3e3aa2dbfc832dd96
Ossama Harach <oharach@temenos.com>
2019-06-21 15:09:27 +0200

    [FEATURE] DB Connection check and GET all users

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 // Create express app
 var express = require("express")
 var app = express()
+var sqlite3 = require('sqlite3').verbose()
+
+// DB Connection
+let db = new sqlite3.Database("mydb.sqlite", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.log("Create DB first with : npm run init")
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQLite database.')
+    }
+});
 
 // Server port
 var HTTP_PORT = 8000 
@@ -13,7 +26,21 @@ app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
 
-// Insert here other API endpoints
+// API endpoints
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from Users"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
 
 // Default response for any other request
 app.use(function(req, res){
